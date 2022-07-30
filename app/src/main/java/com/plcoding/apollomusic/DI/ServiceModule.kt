e6ffac5d7b341abe6fb776b1917630f5130428e1
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2022-07-30 16:48:53 +0100

    Adding in Service Module

---
+++ app/src/main/java/com/plcoding/apollomusic/DI/ServiceModule.kt
@@ -0,0 +1,50 @@
+package com.plcoding.apollomusic.DI
+
+import android.content.Context
+import com.bumptech.glide.util.Util
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.audio.AudioAttributes
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util.getUserAgent
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ServiceScoped
+
+//plays the music
+
+@Module
+@InstallIn(ServiceComponent::class)
+
+
+object ServiceModule {
+
+    @Provides
+    @ServiceScoped
+    fun provideAudioAttributes() = AudioAttributes.Builder()
+        .setContentType(C.CONTENT_TYPE_MUSIC)
+        .setUsage(C.USAGE_MEDIA)
+        .build()
+
+
+    @Provides
+    @ServiceScoped
+    fun provideExoPlayer(
+        @ApplicationContext context: Context,
+        audioAttributes: AudioAttributes
+    ) = SimpleExoPlayer.Builder(context).build().apply {
+        setAudioAttributes(audioAttributes, true )
+        setHandleAudioBecomingNoisy(true)
+    }
+
+
+    @Provides
+    @ServiceScoped
+    fun provideDataSourceFactory(
+        @ApplicationContext context: Context
+
+    ) = DefaultDataSourceFactory(context, com.google.android.exoplayer2.util.Util.getUserAgent(context, "Apollo App"))
+}
---
+++ app/src/main/java/com/plcoding/apollomusic/SpotifyApplication.kt
@@ -0,0 +1,9 @@
+package com.plcoding.apollomusic
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+
+class SpotifyApplication: Application() {
+}
